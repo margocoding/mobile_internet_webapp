bb1a84b7ad6e1699ec2ff146eaaf8b463a45fbe3
margocoding <danilbashirov0@gmail.com>
2026-06-04 20:41:44 +0500

    Fixes in check-balance

--- src/components/shared/hero/Hero.tsx
+++ src/components/shared/hero/Hero.tsx
@@ -1,15 +1,15 @@
-import {motion} from "framer-motion";
-import {Link, useNavigate} from "react-router-dom";
-import {fadeInUp, fadeInUpStagger} from "../../ui/animation";
+import { motion } from "framer-motion";
+import { Link, useNavigate } from "react-router-dom";
+import { fadeInUp, fadeInUpStagger } from "../../ui/animation";
 import Button from "../../ui/Button";
 import Card from "../../ui/Card";
 import Chip from "../../ui/Chip";
 import Input from "../../ui/Input";
-import {availableCountries} from "../CountriesModal";
+import { availableCountries } from "../CountriesModal";
 
 const Hero = ({
-                  setOpenedModal,
-              }: {
+    setOpenedModal,
+}: {
     setOpenedModal: (data: boolean) => void;
 }) => {
     const navigate = useNavigate();
@@ -24,7 +24,7 @@ const Hero = ({
                 variants={fadeInUp}
                 className="text-6xl max-[762px]:text-3xl font-bold z-10 text-center"
             >
-                Мобильный интернет <br/> за границей для вас <br/> и ваших близких
+                Мобильный интернет <br /> за границей для вас <br /> и ваших близких
             </motion.h1>
 
             <motion.div
@@ -37,13 +37,13 @@ const Hero = ({
             <motion.div variants={fadeInUp} className="z-10 mt-5">
                 <Card className="space-y-3 z-10 mx-4 p-5 rounded-4xl">
                     <Input
-                        leftIcon={<img src="/icons/search.svg" alt="Search"/>}
+                        leftIcon={<img src="/icons/search.svg" alt="Search" />}
                         options={availableCountries.map((country) => ({
                             label: country.name,
                             value: country.id,
                             description: `От ${country.startPrice}/день`,
                             icon: country.miniIcon ? (
-                                <img src={country.miniIcon}/>
+                                <img src={country.miniIcon} className="h-5 w-5 object-cover rounded-full" />
                             ) : undefined,
                         }))}
                         onOptionClick={(value) => navigate(`/tariff/${value}`)}
@@ -61,7 +61,7 @@ const Hero = ({
                                             <img
                                                 src={country.miniIcon}
                                                 alt={country.name}
-                                                className="w-6 h-6 max-md:w-4.5 max-md:h-4.5"
+                                                className="w-6 h-6 object-cover rounded-full max-md:w-4.5 max-md:h-4.5"
                                             />
                                             <span>{country.name}</span>
                                         </div>
@@ -81,7 +81,7 @@ const Hero = ({
                 variants={fadeInUp}
                 src="/images/earth.png"
                 alt="Earth"
-                className="absolute bottom-0 left-1/2 -translate-x-1/2 min-[762px]:w-full max-[762px]:w-225"
+                className="absolute bottom-0 left-1/2 -translate-x-1/2 min-[762px]:w-full max-[762px]:w-235"
             />
         </motion.div>
     );
--- src/store/tariffStore.ts
+++ src/store/tariffStore.ts
@@ -1,4 +1,4 @@
-import {create} from "zustand";
+import { create } from "zustand";
 
 interface TariffState {
     openedModal: boolean;
@@ -7,13 +7,23 @@ interface TariffState {
     setCheckBalanceModal: (data: boolean) => void;
     checkBalanceCountryId: string | null;
     setCheckBalanceCountryId: (data: string) => void;
+    checkBalanceTariffId: number | null;
+    setCheckBalanceTariffId: (data: number) => void;
+
+    checkBalanceTariffType: 'unlimited' | 'fixed' | null
+    setCheckBalanceTariffType: (data: 'unlimited' | 'fixed' | null) => void;
+
 }
 
 export const useTariffStore = create<TariffState>((set) => ({
     openedModal: false,
-    setModal: (data: boolean) => set(() => ({openedModal: data})),
+    setModal: (data: boolean) => set(() => ({ openedModal: data })),
     checkBalanceModal: false,
-    setCheckBalanceModal: (data: boolean) => set(() => ({checkBalanceModal: data})),
+    setCheckBalanceModal: (data: boolean) => set(() => ({ checkBalanceModal: data })),
     checkBalanceCountryId: null,
-    setCheckBalanceCountryId: (data: string) => set(() => ({checkBalanceCountryId: data})),
+    setCheckBalanceCountryId: (data: string) => set(() => ({ checkBalanceCountryId: data })),
+    checkBalanceTariffId: null,
+    setCheckBalanceTariffId: (data: number) => set(() => ({ checkBalanceTariffId: data })),
+    checkBalanceTariffType: null,
+    setCheckBalanceTariffType: (data: 'unlimited' | 'fixed' | null) => set(() => ({ checkBalanceTariffType: data }))
 }));
